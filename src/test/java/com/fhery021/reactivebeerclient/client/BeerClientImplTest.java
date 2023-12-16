a6767f63714d8abef490221815b6d9242833c750
fhery021 <fhery021@gmail.com>
2023-12-16 23:04:52 +0100

    Implemented ListBeers

---
+++ src/test/java/com/fhery021/reactivebeerclient/client/BeerClientImplTest.java
@@ -0,0 +1,48 @@
+package com.fhery021.reactivebeerclient.client;
+
+import com.fhery021.reactivebeerclient.config.WebClientConfig;
+import com.fhery021.reactivebeerclient.model.BeerPagedList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BeerClientImplTest {
+
+    BeerClientImpl beerClient;
+
+    @BeforeEach
+    void setUp() {
+        beerClient = new BeerClientImpl(new WebClientConfig().webClient());
+    }
+
+    @Test
+    void getBeerById() {
+    }
+
+    @Test
+    void listBeers() {
+        Mono<BeerPagedList> beerPagedListMono = beerClient.listBeers(null, null, null, null, null);
+        BeerPagedList beerPagedList = beerPagedListMono.block();
+
+        assertThat(beerPagedList).isNotNull();
+        assertThat(beerPagedList.getSize()).isGreaterThan(0);
+    }
+
+    @Test
+    void createBeer() {
+    }
+
+    @Test
+    void updateBeer() {
+    }
+
+    @Test
+    void deleteBeerById() {
+    }
+
+    @Test
+    void getBeerByUPC() {
+    }
+}
